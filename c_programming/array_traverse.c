8ed7315985d8c7674c5ffc279c78a9dd25eed69e
sagarpd28 <sagarpd28@gmail.com>
2025-11-27 14:11:08 +0530

    initial commit

--- c_programming/array_traverse.c
+++ c_programming/array_traverse.c
@@ -5,16 +5,16 @@ int main() {
     int arr[5];
     int i;
 
-    printf("ener 5 elements \n");
-    for(int i=0;i<5; i++){
-        scanf("%d", arr[5]);
+    printf("Enter 5 elements:\n");
 
+    for(i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]); 
     }
-    printf("array elemnets :\n");
-    for(int i = 0; i<5; i++){
-        printf("arr [%d]= %d\n",i,arr[i]);
+
+    printf("Array elements:\n");
+    for(i = 0; i < 5; i++) {
+        printf("arr[%d] = %d\n", i, arr[i]);
     }
     
     return 0;
 }
-
