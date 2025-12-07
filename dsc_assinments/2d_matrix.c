ed0efb9be0a133a2d54ffa217df7396a732c814b
sagarpd28 <sagarpd28@gmail.com>
2025-12-07 20:18:31 +0530

    initial commit

--- dsc_assinments/2d_matrix.c
+++ dsc_assinments/2d_matrix.c
@@ -12,13 +12,12 @@ int main() {
         }
     }
 
+    printf("\nMatrix is:\n");
     for(int i=0;i<3;i++){
-        int sum = 0;
         for(int j=0;j<3;j++){
             printf("%d\t",matrix[i][j]);
-            sum = sum+ matrix[i][j];
-        }
-        printf("1 | sum is %d\n",sum);
+        }    
+        printf("\n");
     }
     return 0;
 }
