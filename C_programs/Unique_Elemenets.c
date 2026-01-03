e02d169c123d4b8fafd18d7654684c15f23ab4f5
sagarpd28 <sagarpd28@gmail.com>
2026-01-03 15:53:07 +0530

    initial commit

---
+++ C_programs/Unique_Elemenets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int a[100], n, i, j, count;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Unique elements in the array are:\n");
+
+    for (i = 0; i < n; i++) {
+        count = 0;
+        for (j = 0; j < n; j++) {
+            if (a[i] == a[j]) {
+                count++;
+            }
+        }
+        if (count == 1) {
+            printf("%d ", a[i]);
+        }
+    }
+
+    return 0;
+}
