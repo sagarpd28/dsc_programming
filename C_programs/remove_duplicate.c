a952d644d32e72908db9f07e85ee16329f82cf24
sagarpd28 <sagarpd28@gmail.com>
2026-01-03 13:26:54 +0530

    initial commit

---
+++ C_programs/remove_duplicate.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int a[100], n, i, j, k;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    // Remove duplicates
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (a[i] == a[j]) {
+                // Shift elements to left
+                for (k = j; k < n - 1; k++) {
+                    a[k] = a[k + 1];
+                }
+                n--;     // reduce array size
+                j--;     // stay at same index
+            }
+        }
+    }
+
+    printf("Array after removing duplicates:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
