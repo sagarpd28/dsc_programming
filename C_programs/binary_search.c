4b7001768467e17f7a89b2ba20859ce62f0dcb5f
sagarpd28 <sagarpd28@gmail.com>
2026-02-09 18:51:26 +0530

    updated

--- C_programs/binary_search.c
+++ C_programs/binary_search.c
@@ -1,36 +1,43 @@
 #include <stdio.h>
 
 int main() {
-    
-    int a[100], key, n, found=0, i;
+
+    int a[100], key, n, low, high, mid, found = 0;
 
     printf("Enter size of array: ");
     scanf("%d", &n);
 
-    printf("Enter elements of array is:\n");
-    for(i=0;i<n;i++){
+    printf("Enter elements in sorted order:\n");
+    for(int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
 
-    printf("Elements of array is:\n");
-    for(i=0;i<n;i++){
-        printf("%d\n", a[i]);
-    }
+    printf("Enter element to search: ");
+    scanf("%d", &key);
 
-    printf("Enter elements to search: ");
-    scanf("%d",&key);
+    low = 0;
+    high = n - 1;
 
-    for(i=0;i<n;i++){
-        if(a[i]==key){
-            printf("Element found at index: %d\n", i);
-            printf("Element found at position: %d\n", i+1);
-            found =1;
+    while(low <= high) {
+
+        mid = (low + high) / 2;
+
+        if(a[mid] == key) {
+            printf("Element found at index: %d\n", mid);
+            printf("Element found at position: %d\n", mid + 1);
+            found = 1;
             break;
+        }
+        else if(key < a[mid]) {
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
         }
     }
 
-    if(found ==0){
+    if(found == 0)
         printf("Element not found");
-    }
+
     return 0;
 }
