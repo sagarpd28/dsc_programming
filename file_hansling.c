468875de2060c6549711c7662df7394e6b5bc612
sagarpd28 <sagarpd28@gmail.com>
2026-03-02 08:41:23 +0530

    file

--- file_hansling.c
+++ file_hansling.c
@@ -1,19 +1,84 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    
-    FILE *f;
+// Self referential structure
+struct NODE {
+    int data;
+    struct NODE *next;
+};
 
-    f = fopen("input.txt", "w");
-    if(f==NULL){
-        printf("Error openingfile.\n");
-        return 1;
+// Create node
+struct NODE* createNode(int value)
+{
+    struct NODE *newNode = (struct NODE*)malloc(sizeof(struct NODE));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Insert at end
+struct NODE* insertEnd(struct NODE *head, int value)
+{
+    struct NODE *newNode = createNode(value);
+
+    if(head == NULL)
+        return newNode;
+
+    struct NODE *temp = head;
+    while(temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+// Write list to file
+void writeToFile(struct NODE *head)
+{
+    FILE *fp = fopen("output.txt","w");
+
+    struct NODE *temp = head;
+    while(temp != NULL)
+    {
+        fprintf(fp,"%d -> ", temp->data);
+        temp = temp->next;
     }
 
-    fprintf(f, "hi vinay\n");
-    
+    fprintf(fp,"NULL");
+    fclose(fp);
+}
 
-    fclose(f);
+int main()
+{
+    struct NODE *head = NULL;
+    FILE *fp;
+    int n, value, i;
+
+    fp = fopen("input.txt","r");
+
+    if(fp == NULL)
+    {
+        printf("Input file not found\n");
+        return 0;
+    }
+
+    // Read number of nodes
+    fscanf(fp,"%d",&n);
+
+    // Read values
+    for(i=0;i<n;i++)
+    {
+        fscanf(fp,"%d",&value);
+        head = insertEnd(head,value);
+    }
+
+    fclose(fp);
+
+    // Write linked list to output file
+    writeToFile(head);
+
+    printf("Linked List created from file and written to output.txt\n");
 
     return 0;
+
 }
